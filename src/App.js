b0645b4f2b65d2d210cc33ee758c4f521f0a681e
Altin Sadiku <altinsadiku2000@gmail.com>
2021-12-01 19:51:50 +0100

    AddCustomerComponent was added.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CustomersComponent from "./components/Customers.component";
 import HeaderComponent from "./components/Header.component";
 import FooterComponent from "./components/Footer.component";
 import ProductsComponent from "./components/Products.components";
+import AddCustomerComponents from "./components/AddCustomer.components";
 
 function App() {
     return (
@@ -16,6 +17,8 @@ function App() {
                       <Route exact path="/" component={CustomersComponent}/>
                       <Route exact path="/customers" component={CustomersComponent}/>
                       <Route exact path="/products" component={ProductsComponent}/>
+                      <Route exact path="/add-customer" component={AddCustomerComponents}/>
+
 
 
                   </Switch>
--- src/components/Customers.component.js
+++ src/components/Customers.component.js
@@ -8,9 +8,15 @@ class CustomersComponent extends Component {
         this.state = {
             customers: []
         }
+        this.addCustomer = this.addCustomer.bind(this);
+        this.deleteCustomer = this.deleteCustomer.bind(this);
     }
 
     componentDidMount() {
+    }
+
+    addCustomer(){
+        this.props.history.push('/add-customer');
     }
 
 
@@ -32,7 +38,7 @@ class CustomersComponent extends Component {
             <div>
                 <h2 className="text-center">Customers List</h2>
                 <div className="d-flex justify-content-between">
-                    <button className="btn btn-success">
+                    <button className="btn btn-success" onClick={this.addCustomer}>
                         Add Customer
                     </button>
                     <input
